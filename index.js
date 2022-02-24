901fd9c5ce5d17608ecfcf882f922e37e8dff537
Filippa <filippabellange@live.se>
2022-02-24 15:34:11 +0100

    middleware

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    const tokenData = jwt.decode(token, process.env.JWTSECRET);
+    res.locals.loggedIn = true;
+    res.locals.username = tokenData.username;
+  } else {
+    res.locals.loggedIn = false;
+  }
+  next();
+});
+
 app.listen(8000, () => {
   console.log("http://localhost:8000/");
 });
